fd313cafca245e303bcd029a4aafb5f657167ce8
magzim24 <kiillofs2@mail.ru>
2024-03-18 23:25:28 +0700

    sfs
    dfs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Map from './Map'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Map />
+    </div>
+  );
+}
+
